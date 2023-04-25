036d7c2bb09d00b76d731b9b6c825a389a1b7127
Henrique <henriqueferreira.fox@gmail.com>
2023-04-25 15:07:12 -0300

    altera title das páginas

--- rotate.php
+++ rotate.php
@@ -11,7 +11,7 @@ include("tvCore.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./index-rotate.css" />
-    <title>HTML Video Player</title>
+    <title>Patrocine - Media Indoor</title>
 </head>
 
 <body>
